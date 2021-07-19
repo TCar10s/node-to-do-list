2e33ef6636eedc1c46051350cfc01ed909c5ffb5
Carlos Lobo <carloslobo@wposs.com>
2021-07-18 22:31:01 -0500

    Funcionalidades de inquirer

--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -1,28 +1,73 @@
 const inquirer = require('inquirer');
 require('colors');
 
-const questions = [{
+const questions = [
+  {
     type: 'list',
     name: 'option',
     message: 'What would you like to do?',
-    choices: ['opt1', 'opt2', 'opt3']
-}];
+    choices: [
+      {
+        value: 1,
+        name: '1. Create task',
+      },
+      {
+        value: 2,
+        name: '2. List tasks',
+      },
+      {
+        value: 3,
+        name: '3. List completed tasks',
+      },
+      {
+        value: 4,
+        name: '4. List pending tasks',
+      },
+      {
+        value: 5,
+        name: '5. Complete task(s)',
+      },
+      {
+        value: 6,
+        name: '6. Delete task',
+      },
+      {
+        value: 0,
+        name: '0. Leave',
+      },
+    ],
+  },
+];
+
 
 const inquirerMenu = async () => {
+  // console.clear();
 
-    console.clear();
+  // Title
+  console.log('┌─────── •✧✧• ───────┐'.green);
+  console.log('   Select an option');
+  console.log('└─────── •✧✧• ───────┘\n'.green);
 
-    // Title
-    console.log('┌─────── •✧✧• ───────┐'.green);
-    console.log('   Select an option'.green);
-    console.log('└─────── •✧✧• ───────┘\n'.green);
+  const { option } = await inquirer.prompt(questions);
 
-    const opt = await inquirer.prompt(questions);
+  return option;
+};
 
-    return opt;
+const pausa = async () => {
 
+    
+    const question = [
+        {
+            type: 'input',
+            name: 'message',
+            message: `Press ${'ENTER'.green} to continue`
+        }
+    ];
+
+    await inquirer.prompt(question);
 };
 
 module.exports = {
-    inquirerMenu
-}
+  inquirerMenu,
+  pausa
+};
